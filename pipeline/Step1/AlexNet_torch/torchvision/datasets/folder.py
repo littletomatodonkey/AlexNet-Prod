701eb9919a0a6abbb31036d2eade2698c92a8c08
littletomatodonkey <dazhiningsibuqu@163.com>
2021-09-26 11:13:12 +0800

    fix torchvision

--- pipeline/Step1/AlexNet_torch/torchvision/datasets/folder.py
+++ pipeline/Step1/AlexNet_torch/torchvision/datasets/folder.py
@@ -273,11 +273,7 @@ def accimage_loader(path: str) -> Any:
 
 
 def default_loader(path: str) -> Any:
-    from torchvision import get_image_backend
-    if get_image_backend() == 'accimage':
-        return accimage_loader(path)
-    else:
-        return pil_loader(path)
+    return pil_loader(path)
 
 
 class ImageFolder(DatasetFolder):

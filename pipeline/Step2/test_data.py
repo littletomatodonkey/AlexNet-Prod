ea49142949e891e2523d5c44e01539900d5b6e70
littletomatodonkey <dazhiningsibuqu@163.com>
2021-09-28 11:20:14 +0800

    add dataloader check

--- pipeline/Step2/test_data.py
+++ pipeline/Step2/test_data.py
@@ -29,7 +29,7 @@ def build_torch_transform():
     return torch_transform
 
 
-def build_paddle_dataset():
+def build_paddle_data_pipeline():
     sys.path.insert(0, "./AlexNet_paddle/")
     import AlexNet_paddle.presets as presets
     import AlexNet_paddle.paddlevision as paddlevision
@@ -37,11 +37,16 @@ def build_paddle_dataset():
         "/paddle/data/ILSVRC2012_torch/val/",
         presets.ClassificationPresetEval(
             crop_size=224, resize_size=256))
+    test_sampler = paddle.io.SequenceSampler(dataset_test)
+    test_batch_sampler = paddle.io.BatchSampler(
+        sampler=test_sampler, batch_size=32)
+    data_loader_test = paddle.io.DataLoader(
+        dataset_test, batch_sampler=test_batch_sampler, num_workers=0)
     sys.path.pop(0)
-    return dataset_test
+    return dataset_test, data_loader_test
 
 
-def build_torch_dataset():
+def build_torch_data_pipeline():
     sys.path.insert(0, "./AlexNet_torch")
     import AlexNet_torch.presets as presets
     import AlexNet_torch.torchvision as torchvision
@@ -49,8 +54,15 @@ def build_torch_dataset():
         "/paddle/data/ILSVRC2012_torch/val/",
         presets.ClassificationPresetEval(
             crop_size=224, resize_size=256))
+    test_sampler = torch.utils.data.SequentialSampler(dataset_test)
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test,
+        batch_size=32,
+        sampler=test_sampler,
+        num_workers=0,
+        pin_memory=True)
     sys.path.pop(0)
-    return dataset_test
+    return dataset_test, data_loader_test
 
 
 def test_transform():
@@ -64,10 +76,10 @@ def test_transform():
     np.testing.assert_allclose(paddle_img, torch_img)
 
 
-def test_dataset():
+def test_data_pipeline():
     diff_helper = ReprodDiffHelper()
-    paddle_dataset = build_paddle_dataset()
-    torch_dataset = build_torch_dataset()
+    paddle_dataset, paddle_dataloader = build_paddle_data_pipeline()
+    torch_dataset, torch_dataloader = build_torch_data_pipeline()
 
     logger_paddle_data = ReprodLogger()
     logger_torch_data = ReprodLogger()
@@ -78,14 +90,22 @@ def test_dataset():
     # random choose 5 images and check
     for idx in range(5):
         rnd_idx = np.random.randint(0, len(paddle_dataset))
-        logger_paddle_data.add(f"data_{idx}",
+        logger_paddle_data.add(f"dataset_{idx}",
                                paddle_dataset[rnd_idx][0].numpy())
-        logger_torch_data.add(f"data_{idx}",
+        logger_torch_data.add(f"dataset_{idx}",
                               torch_dataset[rnd_idx][0].detach().cpu().numpy())
+
+    for idx, (paddle_batch, torch_batch
+              ) in enumerate(zip(paddle_dataloader, torch_dataloader)):
+        if idx >= 5:
+            break
+        logger_paddle_data.add(f"dataloader_{idx}", paddle_batch[0].numpy())
+        logger_torch_data.add(f"dataloader_{idx}",
+                              torch_batch[0].detach().cpu().numpy())
 
     diff_helper.compare_info(logger_paddle_data.data, logger_torch_data.data)
     diff_helper.report()
 
 
 if __name__ == "__main__":
-    test_dataset()
+    test_data_pipeline()
